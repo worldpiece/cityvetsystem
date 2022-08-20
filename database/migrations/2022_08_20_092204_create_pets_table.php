71bd7baf715f6e54c948fabf31adf79020c9bdac
rnnlbrsl <ronnel.brosola@gmail.com>
2022-08-20 17:27:50 +0800

    initial commit

---
+++ database/migrations/2022_08_20_092204_create_pets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->incerements('pet_id');
+            $table->string('pet_name');
+            $table->int('owner_id');
+            $table->int('pet_classification_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pets');
+    }
+}
