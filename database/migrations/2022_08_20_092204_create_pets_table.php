4be5cc43e9612c4736e06a79784b62f01ed2124d
rnnlbrsl <ronnel.brosola@gmail.com>
2022-08-20 18:17:38 +0800

    added the pet page and vue ui

--- database/migrations/2022_08_20_092204_create_pets_table.php
+++ database/migrations/2022_08_20_092204_create_pets_table.php
@@ -14,10 +14,10 @@ class CreatePetsTable extends Migration
     public function up()
     {
         Schema::create('pets', function (Blueprint $table) {
-            $table->incerements('pet_id');
+            $table->increments('pet_id');
             $table->string('pet_name');
-            $table->int('owner_id');
-            $table->int('pet_classification_id');
+            $table->integer('owner_id');
+            $table->integer('pet_classification_id');
             $table->timestamps();
         });
     }
---
+++ resources/views/pet/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Pet</title>
+<link rel="stylesheet" href="{{ asset('css/app.css')}}">
+</head>
+<body>
+<div id="app">
+<!-- As a heading -->
+<nav class="navbar navbar-light bg-light">
+<span class="navbar-brand mb-0 h1">Alaminos City Veterinary System</span>
+</nav>
+<h1>Pet Page </h1>
+<example-component />
+</div>
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
