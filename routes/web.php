30b51db67faa32a8926e481d1ddfe1bde500da50
benzar <ofwreck17@gmail.com>
2022-09-17 10:35:29 +0800

    routes update

--- routes/web.php
+++ routes/web.php
@@ -22,31 +22,25 @@ Route::get('/', function () {
   return view('pet.ownerRegister');
 });
 
+Route::group(['middleware' => 'auth'], function () {
 Route::post('/saveOwner', [OwnerRegController::class, 'saveOwner'])->name('saveOwner');
-
 Route::post('/updateOwner/{id}', [OwnerRegController::class, 'updateOwner'])->name('updateOwner');
-
 Route::post('/deleteOwner/{id}', [OwnerRegController::class, 'deleteOwner'])->name('deleteOwner');
-
 Route::post('/deleteAllOwner', [OwnerRegController::class, 'deleteAllOwner'])->name('deleteAllOwner');
-
 Route::post('/updateOwnerSaved{id}', [OwnerRegController::class, 'updateOwnerSaved'])->name('updateOwnerSaved');
-
 Route::get('/viewOwnerList', [OwnerRegController::class, 'viewOwnerList']);
-
 Route::get('/petOwned/{id}', [PetController::class, 'petOwned'])->name('petOwned');
-
 Route::get('/ownerDashboard/{id}', [OwnerRegController::class, 'ownerDashboard'])->name('ownerDashboard');
-
 Route::resource('pet', PetController::class);
-
 Route::post('/petRegister/{id}' , [PetController::class, 'petRegister'])->name('petRegister');
-
 Route::post('/savePet/{id}', [PetController::class, 'savePet'])->name('savePet');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']); //->name('home');
+});
+
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']); //->name('home');
+
 Route::get('/appointment', [App\Http\Controllers\AppointmentController::class, 'index']);
 
 Route::get('/appointment', [AppointmentController::class, 'index']);
