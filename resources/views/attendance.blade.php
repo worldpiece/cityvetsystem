3cd36e5bb19c54c69838337e8fd3fbf58e7a526e
rnnlbrsl <ronnel.brosola@gmail.com>
2022-11-14 21:46:22 +0800

    fixed qrcode scanner

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -1,57 +1,65 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Attendance') }}</div>
-                    <div class="card-body">
-                        <div class="justify-content-center">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                            <div class="form-group mb-2 p-0">
-                                <video id="preview" class="form-control p-0"></video>
-                            </div>
-                            <input type="text" name="name" id="name">
-                        </div>
-                        <form>
-                            <br>
-                            <div class="form-row d-flex justify-content-center">
-                                <div class="col-auto my-1">
-                                    <button type="submit" class="btn btn-primary" id="time-in">Time-In</button>
-                                    &nbsp;
-                                    &nbsp;
-                                    &nbsp;
-                                    <button type="submit" class="btn btn-primary" id="time-out">Time-Out</button>
-                                </div>
-                            </div>
-                        </form>
+    <title>Laravel</title>
 
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/3.3.3/adapter.min.js"></script>
+    <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+    <!-- Styles -->
+</head>
+
+<body class="antialiased">
+    <div
+        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+
+            <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                <div>
+                    <div class="col-md-6 text-center">
+                        <video id="preview" width="500px"></video>
                     </div>
+                    <input type="text" name="name" id="name">
                 </div>
+                <form>
+                    <br>
+                    <div class="form-row d-flex justify-content-center">
+                        <div class="col-auto my-1">
+                            <button type="submit" class="btn btn-primary" id="time-in">Time-In</button>
+                            &nbsp;
+                            &nbsp;
+                            &nbsp;
+                            <button type="submit" class="btn btn-primary" id="time-out">Time-Out</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-@endsection
+</body>
+<script type="text/javascript">
+    let scanner = new Instascan.Scanner({
+        video: document.getElementById('preview')
+    });
+    scanner.addListener('scan', function(content) {
+        console.log(content);
+    });
+    Instascan.Camera.getCameras().then(function(cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+        } else {
+            console.error('No cameras found.');
+        }
+    }).catch(function(e) {
+        console.error(e);
+    });
+</script>
 
-
-@section('scripts')
-    <script type="text/javascript">
-        let scanner = new Instascan.Scanner({
-            video: document.getElementById('preview')
-        });
-        Instascan.Camera.getCameras().then(function(cameraas) {
-            if (cameras.length > 0) {
-                scanner.start(cameras[0]);
-            } else {
-                alert('No cameras found!');
-            }
-        }).catch(function(e) {
-            console.console.error(e);
-        });
-
-        scanner.addListener('scan', function(c) {
-            document.getElementById('name').value = c;
-        });
-    </script>
-@endsection
+</html>
---
+++ resources/views/layouts/attendance.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title', 'Attendance')</title>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/custom.js') }}" defer></script>
+    <script src="https://www.google.com/recaptcha/api.js?" async defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    {{-- QR Scanner --}}
+    <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/3.3.3/adapter.min.js"></script>
+
+    {{-- Font Awesome --}}
+    <script src="https://kit.fontawesome.com/24bf8aae18.js" crossorigin="anonymous"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app">
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+
+</html>
